9551a305d23da391ef347d936f4db47c2cbbd228
Ngufor emma <98039632+Ngufor-emmanuella@users.noreply.github.com>
2023-02-11 13:00:05 +0100

    Add changes as requested by reviewer

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,6 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
 
   def update_comment_counter
-    comment.increment!(:comments_counter)
+    post.increment!(:comments_counter)
   end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -3,6 +3,6 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
 
   def update_likes_counter
-    like.increment!(:likes_counter)
+    post.increment!(:likes_counter)
   end
 end
