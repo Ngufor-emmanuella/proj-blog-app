a206e11bf2220e4d06a68b61d312c6bf69421130
Ngufor emma <98039632+Ngufor-emmanuella@users.noreply.github.com>
2023-02-15 20:49:28 +0100

    Add view

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,4 @@ Rails.application.routes.draw do
   get 'users/:id', to: 'users#show', as: 'user'
   get 'users/:user_id/posts', to: 'posts#index', as: 'user_posts'
   get 'users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
-
 end
