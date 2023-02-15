0676f89cd6f728a89b59718644412b0c8e6638ea
Ngufor emma <98039632+Ngufor-emmanuella@users.noreply.github.com>
2023-02-15 07:23:44 +0100

    Add test to contollers

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,54 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  subject { Post.new(title: 'my post', text: 'post something nice', comments_counter: 12, likes_counter: 7) }
+  subject(:post) { Post.create(title: 'Test post', comments_counter: 5, likes_counter: 20) }
 
-  before { subject.save }
-
-  it 'expects the post title to be present' do
+  it 'Title should be present' do
     subject.title = nil
     expect(subject).to_not be_valid
   end
 
-  it 'expects the post title to not be  long' do
-    subject.title = 'abc' * 200
+  it 'Title should not be too long' do
+    subject.title = 'a' * 251
     expect(subject).to_not be_valid
   end
 
-  it 'expects the post title to not be too short' do
-    subject.title = 'a'
+  it 'Comments counter should be an integer' do
+    subject.comments_counter = 1.5
     expect(subject).to_not be_valid
   end
 
-  it 'expects the post title to have valid value' do
-    expect(subject.title).to eql 'my post'
-  end
-
-  it 'expects text to be present' do
-    subject.text = nil
-    expect(subject).to_not be_valid
-  end
-
-  it 'expects text to have valid a value' do
-    expect(subject.text).to eql 'post something nice'
-  end
-
-  it 'expects the comments_counter to be an integer' do
-    subject.comments_counter = 'Ten'
-    expect(subject).to_not be_valid
-  end
-
-  it 'expects comments_counter to not be less than 1' do
+  it 'Comments counter should be greater than or equal to 0' do
     subject.comments_counter = -1
     expect(subject).to_not be_valid
   end
 
-  it 'expects the likes_counter to be an integer' do
-    subject.likes_counter = 'Seven'
+  it 'Likes counter should be an integer' do
+    subject.likes_counter = 1.5
     expect(subject).to_not be_valid
   end
 
-  it 'expects the likes_counter must not be less than 1' do
+  it 'Likes counter should be greater than or equal to 0' do
     subject.likes_counter = -1
     expect(subject).to_not be_valid
   end
