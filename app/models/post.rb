e641025b0c5d87c48dbc5686a43b346be578a913
Ngufor emma <98039632+Ngufor-emmanuella@users.noreply.github.com>
2023-02-16 15:23:12 +0100

    Rmove duplicates file

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,9 +3,9 @@ class Post < ApplicationRecord
   has_many :likes, class_name: 'Like', foreign_key: :post_id
   belongs_to :author, class_name: 'User', foreign_key: :author_id
 
-  validates :title, presence: true, length: { maximum: 250 }
-  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  # validates :title, presence: true, length: { maximum: 250 }
+  # validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  # validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   after_save :update_post_count
 
   def update_post_count
