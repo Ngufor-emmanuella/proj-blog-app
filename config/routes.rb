51742bcaeca0c257ab1340d09178a0ef623aa3f9
Ngufor emma <98039632+Ngufor-emmanuella@users.noreply.github.com>
2023-02-18 13:47:34 +0100

    Add data base

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/show'
-  root 'users#index'
-  get 'users', to: 'users#index'
-  get 'users/:id', to: 'users#show', as: 'user'
-  get 'users/:user_id/posts', to: 'posts#index', as: 'user_posts'
-  get 'users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [:create]
+    end
+  end
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root 'users#index', as: 'home'
 end
